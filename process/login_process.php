7b33f10b715e644365f2bd775b8abdf7ead00c3c
ricckimislang <dragonmislang9@gmail.com>
2024-10-08 13:07:52 +0800

    login admin

--- process/login_process.php
+++ process/login_process.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include '../includes/dbconn.php';
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
@@ -7,16 +8,28 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
 
 
-    $user = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-    $result = mysqli_query($conn, $user);
+    $user = "SELECT * FROM users WHERE username = ? AND password = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $user)) {
+        echo 'sqlerror';
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
     if ($row) {
         $_SESSION['username'] = $username;
-        header('Location: survey.php');
-        exit;
+        echo '1';
+        exit();
     } else {
-        header('Location: admin.php?error=1');
-        exit;
+        echo '0';
+        exit();
     }
+
+
 }
+
+
 
