22fb35daa97e83846faea44c795c6dbe2d73cf33
ricckimislang <dragonmislang9@gmail.com>
2024-10-17 14:30:28 +0800

    NEW UI

---
+++ admin/includes/newsidebar.php
@@ -0,0 +1,31 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link " href="#">
+                <i class="bi bi-card-text"></i>
+                <span>Create Survey</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link " href="#">
+                <i class="bi bi-check2-square"></i>
+                <span>Completed Survey</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link " href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+    </ul>
+
+</aside><!-- End Sidebar-->
